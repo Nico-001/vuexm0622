cf2e4778151d2c294670d00abd394703117cb75b
zhutianzun <zhutianzun@ysstech.com>
2020-10-29 14:42:44 +0800

    oneday

---
+++ src/views/Search/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Search
+    param:{{$route.params.keyword}} --- query:{{$route.query.keyword1}} <br>
+
+    param:{{keyword}} --- query:{{keyword1}}
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Search',
+    props:['keyword','keyword1']
+  
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
