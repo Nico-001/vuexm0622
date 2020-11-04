8918ebcf837db4e4ab4fe8db53237f88344468c2
zhutianzun <zhutianzun@ysstech.com>
2020-11-04 23:19:27 +0800

    6

---
+++ src06/ajax/Ajax.js
@@ -0,0 +1,30 @@
+//导入axios
+import axios from 'axios'
+
+//导入Nprogress
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+//
+const instance = axios.create({
+  baseURL:'/api',
+  timeout:20000
+})
+
+instance.interceptors.request.use((config)=>{
+  Nprogress.start()
+  return config
+})
+
+instance.interceptors.response.use(
+  (response)=>{
+    Nprogress.done()
+    return response.data
+  },
+  (error)=>{
+    Nprogress.done()
+    alert('请求失败'+ error.message || '未知错误')
+    return new Promise(()=>{})
+  })
+
+  export default instance
---
+++ src06/api/index.js
@@ -0,0 +1,33 @@
+
+import Ajax from '@/ajax/Ajax'
+import mockAjax from '@/ajax/mockAjax'
+
+export const reqCategoryList = () =>{
+    return Ajax({
+      url:'/product/getBaseCategoryList',
+      mothod:'get'
+    })
+}
+// reqCategoryList()
+
+export const reqBannerList = () =>{
+  return mockAjax({
+    url:'/banner',
+    mothod:'get'
+  })
+}
+export const reqFloorList = () =>{
+  return mockAjax({
+    url:'/floor',
+    mothod:'get'
+  })
+}
+
+export const reqGoodsListInfo = (searchParams) => {
+  return Ajax({
+    url:'/list',
+    method:'post',
+    data:searchParams
+  })
+}
+
